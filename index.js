693ebf9787760a74636726ab304ed17c37e80587
Josh Dunn <jdunn@digitalbazaar.com>
2020-04-24 11:58:51 -0600

    Add BrQTable, BrQModal and BrQTitleCard.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-//export ... from './BrQ....vue';
+
+'use strict';
+
+export {default as BrQTable} from './BrQTable.vue';
+export {default as BrQModal} from './BrQModal.vue';
+export {default as BrQTitleCard} from './BrQTitleCard.vue';
