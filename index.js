c36912f70f9b1e8b4d56ac0ed19f44e8b5f2e058
David I. Lehn <dil@lehn.org>
2020-04-29 14:57:37 -0400

    Sort components.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 'use strict';
 
-export {default as BrQTable} from './BrQTable.vue';
 export {default as BrQModal} from './BrQModal.vue';
+export {default as BrQTable} from './BrQTable.vue';
 export {default as BrQTitleCard} from './BrQTitleCard.vue';
