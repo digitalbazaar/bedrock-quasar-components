72a34cfba9e9a13d0f6b642a24bfa47243557395
David I. Lehn <dlehn@digitalbazaar.com>
2020-06-05 20:33:49 -0400

    Cleanup.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-
-'use strict';
-
 export {default as BrQModal} from './BrQModal.vue';
 export {default as BrQTable} from './BrQTable.vue';
 export {default as BrQTitleCard} from './BrQTitleCard.vue';
